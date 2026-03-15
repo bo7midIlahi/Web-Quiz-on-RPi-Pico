f67fd821dae42c81424b93bbc76ba0d56f370125
bohmid <elbo7ilahi@gmail.com>
2026-03-15 15:23:39 +0100

    add: function to fetch questions

--- web/game.js
+++ web/game.js
@@ -2,3 +2,19 @@ export function main() {
   //export to make the function accessible from script.js
   console.log("main function");
 }
+
+async function loadQuestions(topic) {
+  try {
+    console.log("loading", topic);
+    const response = await fetch(`../questions/${topic}.json`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error loading questions:", error);
+  }
+}
+// usage
+
+loadQuestions("html").then((q) => console.log(q));
+loadQuestions("css").then((q) => console.log(q));
+loadQuestions("js").then((q) => console.log(q));
